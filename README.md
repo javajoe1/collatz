978c0f0ea0c626137bae969f8ca57282f17a2ef7
Joe Fisher <javajoe@churchofjesuschrist.org>
2019-12-06 08:12:44 -0700

    inital project

---
+++ README.md
@@ -0,0 +1,1 @@
+# An attempt to process the collatz conjecture
---
+++ src/main/java/com/gws/Collatz.java
@@ -0,0 +1,10 @@
+package com.gws;
+
+import java.util.logging.Logger;
+
+public class Collatz {
+
+    public static void main(String... args) {
+        Logger logger=Logger.getLogger(Collatz.class.getName());
+        logger.info("This is a module-using Hello World!");    }
+}
